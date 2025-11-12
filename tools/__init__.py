18497c0ea3d1667cdfd5841a6204582b59ff9de7
Moreira, Guilherme Valler <gumoreira+deloitte@deloitte.pt>
2025-11-12 19:24:48 +0000

    remove unused context tool

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,5 @@
 from .flight_tool import search_oneway_flights, search_roundtrip_flights
-from .context_tool import extract_travel_context
 from .hotel_tool import get_ranked_hotels_near_client
 
 flight_tools: list = [search_oneway_flights, search_roundtrip_flights]
-context_tools: list = [extract_travel_context]
 hotel_tools: list = [get_ranked_hotels_near_client]
