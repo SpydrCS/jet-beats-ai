91829a18fa84f36d4c8b2d20b3ff6d2247c7d378
Moreira, Guilherme Valler <gumoreira+deloitte@deloitte.pt>
2025-11-13 11:42:39 +0000

    add flight agent example prompt and output

---
+++ prompts/input/flight_agent.py
@@ -0,0 +1,7 @@
+INPUT_PROMPT_1 = {
+    "origin_airport": "LIS",
+    "destination_airport": "CDG",
+    "departure_date": "2026-01-20",
+    "return_date": "2026-01-24",
+    "trip_type": "round-trip",
+}
