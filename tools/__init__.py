a073921a9ba601089b176679fe7efe7fa69a35e2
Moreira, Guilherme Valler <gumoreira+deloitte@deloitte.pt>
2025-11-13 11:52:09 +0000

    fix hotel_tool import

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,5 @@
 from .flight_tool import search_oneway_flights, search_roundtrip_flights
-from .hotel_tools import get_hotels_near_destination
+from .hotel_tool import get_hotels_near_destination
 
 flight_tools: list = [search_oneway_flights, search_roundtrip_flights]
 hotel_tools: list = [get_hotels_near_destination]
